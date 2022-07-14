06a3980326004e928589dc7e1b2f8c2cf96e54fe
Ilya Tovpeko <mcilyasat@mail.ru>
2022-07-14 12:24:27 +0300

    added base server functionality

--- server.js
+++ server.js
@@ -5,3 +5,19 @@ require("dotenv").config();
 const path = require("path");
 
 const app = express();
+
+
+mongoose
+    .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to DB"))
+    .catch((error) => console.log(error));
+
+
+app.listen(process.env.PORT, (error) => {
+    error ? console.log(error) : console.log(`Server is runnig on port ${process.env.PORT}`);
+});
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
